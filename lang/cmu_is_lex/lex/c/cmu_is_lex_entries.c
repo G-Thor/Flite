adba740e3c01a2c0c9763065675083f94b9cda44
Gunnar Thor <ornolfsson@gmail.com>
2021-09-08 10:33:24 +0000

    Convert lexical entries, compress and copy to dir

---
+++ lang/cmu_is_lex/lex/c/cmu_is_lex_entries.c
@@ -0,0 +1,92 @@
+/*******************************************************/
+/**  generated lexicon entry file from cmu_is    */
+/*******************************************************/
+
+#include "cst_string.h"
+#include "cst_lexicon.h"
+
+const int cmu_is_lex_num_bytes = 
+#include "cmu_is_lex_num_bytes.c"
+;
+
+const int cmu_is_lex_num_entries = 19271;
+
+const char * const cmu_is_lex_phone_table[61] = 
+{
+    "_epsilon_",
+    "pau",
+    "a1",
+    "k",
+    "r_0",
+    "E1",
+    "v",
+    "l",
+    "au1",
+    "m",
+    "t",
+    "x",
+    "r",
+    "s",
+    "p",
+    "n",
+    "Y",
+    "N_0",
+    "ei1",
+    "f",
+    "a:1",
+    "O1",
+    "D",
+    "G",
+    "I1",
+    "i1",
+    "N",
+    "c",
+    "9",
+    "h",
+    "n_0",
+    "u1",
+    "C",
+    "j",
+    "k_h",
+    "ai1",
+    "ou1",
+    "l_0",
+    "t_h",
+    "J",
+    "T",
+    "c_h",
+    "m_0",
+    "p_h",
+    "Oi1",
+    "9i",
+    "9i:",
+    "Yi",
+    "J_0",
+    "E:1",
+    "ei:1",
+    "I:1",
+    "au:1",
+    "ou:1",
+    "u:1",
+    "ai:1",
+    "9:",
+    "i:1",
+    "O:1",
+    "Y:",
+    NULL
+};
+
+const char * const cmu_is_lex_phones_huff_table[257] = 
+{
+    NULL, /* reserved */
+#include "cmu_is_lex_phones_huff_table.c"
+    NULL
+};
+
+const char * const cmu_is_lex_entries_huff_table[257] = 
+{
+    NULL, /* reserved */
+#include "cmu_is_lex_entries_huff_table.c"
+    NULL
+};
+
